4b684ed655090f639bd4ead707f9d0e8209fe493
dieum512 <akonkwadieum222@gmail.com>
2022-12-20 17:37:30 +0200

    correct linters errors

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.header-ul');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-})
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    menu.classList.remove('active');
-}))
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menu.classList.remove('active');
+}));
