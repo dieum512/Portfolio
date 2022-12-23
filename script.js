a90433403b71cc7871db28a4643128acc0b03796
dieum512 <akonkwadieum222@gmail.com>
2022-12-23 15:44:06 +0200

    Add logic for the popup window

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+import data from './datainfo.js';
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.header-ul');
+
+const myProjects = data.projects;
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
@@ -10,3 +14,152 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   menu.classList.remove('active');
 }));
+
+const body = document.querySelector('body');
+const section = document.createElement('section');
+const cardGrid = document.querySelector('.works .works-list');
+const cardGridArticle = document.createElement('article');
+function implementProjects() {
+  myProjects.forEach((project) => {
+    const projectArticle = `
+    <div class="first-work">
+    <div class="image area-B">
+      <img
+        src="${project.projectImage}"
+        alt="multi-Post project picture"
+      />
+    </div>
+    <div class="info area-A">
+      <h2 class="info-tittle">${project.projectName}</h2>
+      <ul class="ul-info">
+        <li class="canopy">${project.companyName}</li>
+        <li>
+          <img src="images/Counter.svg" alt="counter icon" />
+        </li>
+        <li class="back-end">${project.projectPosition}</li>
+        <li>
+          <img src="images/Counter.svg" alt="counter icon" />
+        </li>
+        <li class="year">2015</li>
+      </ul>
+      <p class="paragraph">
+        ${project.projectDescription}
+      </p>
+      <ul class="ul-languages" aria-label=" Programming languages">
+        <li>${project.projectTechHtml}</li>
+        <li>${project.projecttechCss}</li>
+        <li>${project.projectTechJs}</li>
+      </ul>
+      <div>
+        <a href="#" class="see-project" data-key="${project.key}">See Project</a>
+      </div>
+    </div>
+  </div>
+    `;
+    // seeProject.dataset.key=`${project.key}`
+    cardGridArticle.innerHTML += projectArticle;
+    cardGrid.append(cardGridArticle);
+  });
+}
+
+implementProjects();
+
+body.appendChild(section);
+
+const openModal = [...document.querySelectorAll('.see-project')];
+const projectNameInPopUp = document.querySelector('.projectnameinpopop');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal');
+const popUpGridArticle = document.createElement('article');
+
+const showPopup = (btn) => {
+  modal.classList.toggle('active');
+  overlay.classList.toggle('active');
+  let projectPortfolio = myProjects.filter((project) => project.key === Number(btn.dataset.key));
+  [projectPortfolio] = projectPortfolio;
+  projectNameInPopUp.innerHTML = projectPortfolio.projectName;
+  const popUpArticle = `
+  
+  
+  <div>
+    <ul class="ul-info-1">
+      <li class="canopy-1">${projectPortfolio.companyName}</li>
+      <li>
+        <img src="images/Counter.svg" alt="counter icon" />
+      </li>
+      <li class="back-end-1">${projectPortfolio.projectPosition}</li>
+      <li>
+        <img src="images/Counter.svg" alt="counter icon" />
+      </li>
+      <li class="year-1">${projectPortfolio.projectDate}</li>
+    </ul>
+  </div>
+  <div class="img-1">
+    <img
+      src="${projectPortfolio.projectImage}"
+      alt="multi-Post project picture"
+    />
+  </div>
+  <div class="grid-it">
+    <div class="paragraph-div-1">
+      <p class="paragraph-1">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of
+        type and scrambled it to make a type specimen book. It has
+        survived not only five centuries, but also the leap into
+        electronic typesetting, remaining essent
+      </p>
+    </div>
+    <div class="languages-and-btn-div-1">
+      <div class="languages-div-1">
+        <div>
+          <ul
+            class="ul-languages-1 number-1"
+            aria-label=" Programming languages"
+          >
+            <li>html</li>
+            <li>css</li>
+            <li>javaScript</li>
+          </ul>
+        </div>
+        <div>
+          <ul
+            class="ul-languages-1 number-2"
+            aria-label=" Programming languages"
+          >
+            <li>github</li>
+            <li>ruby</li>
+            <li>bootstraps</li>
+          </ul>
+        </div>
+      </div>
+      <div class="buttons-div">
+        <div class="see-live-div">
+          <button>See live</button>
+        </div>
+        <div class="source-div">
+          <button>See Source</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+`;
+  popUpGridArticle.innerHTML = popUpArticle;
+
+  modal.appendChild(popUpGridArticle);
+};
+
+openModal.forEach((n) => n.addEventListener('click', () => showPopup(n)));
+
+closeModal.addEventListener('click', () => {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
